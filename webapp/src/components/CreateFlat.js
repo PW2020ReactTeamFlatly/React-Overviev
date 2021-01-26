287a10ed14c56ff266030a885ca67cce5d0dbcc9
adamskas <72467341+adamskas@users.noreply.github.com>
2021-01-26 14:42:10 +0100

    createFlatComponent (#7)

--- webapp/src/components/CreateFlat.js
+++ webapp/src/components/CreateFlat.js
@@ -1,7 +1,303 @@
-import React from "react";
+// import React from "react";
 
-export default function CreateFlat() {
+// export default function CreateFlat() {
+//     return (
+//         <div> CreateFlat </div>
+//     );
+// }
+
+import React, { useState, useEffect, useContext } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Paper from '@material-ui/core/Paper';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import Link from '@material-ui/core/Link';
+import axios from 'axios';
+import SnackbarContext from '../contexts/SnackbarContext';
+import LoadingContext from '../contexts/LoadingContext';
+import { Link as RouterLink } from 'react-router-dom';
+//import AddressForm from './AddressForm';
+//import PaymentForm from './PaymentForm';
+//import Review from './Review';
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+      position: 'relative',
+    },
+    layout: {
+      width: 'auto',
+      marginLeft: theme.spacing(2),
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+        width: 600,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      },
+    },
+    paper: {
+      marginTop: theme.spacing(3),
+      marginBottom: theme.spacing(3),
+      padding: theme.spacing(2),
+      [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+        marginTop: theme.spacing(6),
+        marginBottom: theme.spacing(6),
+        padding: theme.spacing(3),
+      },
+    },
+    stepper: {
+      padding: theme.spacing(3, 0, 5),
+    },
+    buttons: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+    },
+    button: {
+      marginTop: theme.spacing(3),
+      marginLeft: theme.spacing(1),
+    },
+  }));
+  
+  const steps = ['New flat'];
+  
+//   function getStepContent(step) {
+//     switch (step) {
+//       case 0:
+//         return <AddressFormm />;
+//       case 1:
+//         return <PaymentForm />;
+//       case 2:
+//         return <Review />;
+//       default:
+//         throw new Error('Unknown step');
+//     }
+//   }
+  
+  export default function Checkout() {
+    const classes = useStyles();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [name, setName] = React.useState("");
+    const [address, setAddress] = React.useState("");
+    const [city, setCity] = React.useState("");
+    const [price, setPrice] = React.useState("");
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [info, setInfo] = React.useState("");
+    const [posted, setPosted] = useState(true);
+    const { setLoading } = useContext(LoadingContext);
+    const { setSnackbar } = useContext(SnackbarContext);
+
+    const handleNameChange = (event) =>{
+        setName(event.target.value);
+        btnEnablingCheck();
+    }
+
+    const handleAddressChange = (event) =>{
+        setAddress(event.target.value);
+        btnEnablingCheck();
+    }
+    const handleCityChange = (event) =>{
+        setCity(event.target.value);
+        btnEnablingCheck();
+    }
+    const handlePriceChange = (event) =>{
+        setPrice(event.target.value);
+        btnEnablingCheck();
+    }
+
+    const handleInfoChange = (event) =>{
+      setInfo(event.target.value);
+      btnEnablingCheck();
+  }
+
+    const btnEnablingCheck = () =>
+    {
+      if(name.length>0 && address.length>0 && city.length>0 && price.length>0 && info.length>0)
+        setBtnDisabled(false);
+    }
+
+    const headers = {
+      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
+      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, X-Auth-Token, Origin, Authorization',
+      'security-header': 'secureMe'
+    }
+
+
+const variable = [{
+  name:name,
+  city:city,
+  pricePerNight:price,
+  address:address,
+  information:info,
+}];
+
+    const handleNext = async () => {
+
+
+      console.log(name);
+        try
+        {       
+        await axios.post('http://localhost:8080/flats',variable);
+        }
+        catch(error) {
+          setPosted(false);
+          setSnackbar({
+            open: true,
+            type: 'error',
+            message: 'Not able to post new flat'
+        });
+    }
+      
+      setActiveStep(activeStep + 1);
+    };
+  
+    const handleBack = () => {
+      setActiveStep(activeStep - 1);
+    };
+  
     return (
-        <div> CreateFlat </div>
+      <React.Fragment>
+        <CssBaseline />
+        <AppBar position="absolute" color="default" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              Add flat view
+            </Typography>
+
+            
+            <Button
+            variant="contained"
+            color="primary" 
+            style={{ marginLeft: "auto" }}
+            component={RouterLink} to="/flatlist">
+              Back
+            </Button>
+            
+          </Toolbar>
+        </AppBar>
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Typography component="h1" variant="h4" align="center">
+            New flat
+            </Typography>
+            {/* <Stepper activeStep={activeStep} className={classes.stepper}>
+              {steps.map((label) => (
+                <Step key={label}>
+                  <StepLabel>{label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper> */}
+            <React.Fragment>
+              {activeStep === steps.length ? (
+                
+                <React.Fragment>
+                  <Typography variant="h5" gutterBottom>
+                  {posted === true ? "Thank you for adding new flat.":"Unfortunetly there was a problem with adding new flat."}
+                  </Typography>
+                  
+                  
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  <React.Fragment>
+      {/* <Typography variant="h6" gutterBottom>
+        Flat information
+      </Typography> */}
+      <Grid container spacing={3}>
+
+        <Grid item xs={12} >
+          <TextField
+            required
+            id="name"
+            name="name"
+            label="Name"
+            fullWidth
+            value={name}
+            onChange={(event) => handleNameChange(event)}            
+          />
+        </Grid>
+        
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="address"
+            name="address"
+            label="Address"
+            fullWidth
+            autoComplete="shipping address-line1"
+            value={address}
+            onChange={(event) => handleAddressChange(event)} 
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="city"
+            name="city"
+            label="City"
+            fullWidth
+            autoComplete="shipping address-level2"
+            value={city}
+            onChange={(event) => handleCityChange(event)} 
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField 
+            id="pricePerNight" 
+            name="pricePerNight" 
+            label="Price per night" 
+            fullWidth 
+            required
+            value={price}
+            onChange={(event) => handlePriceChange(event)} 
+          />
+        </Grid>
+
+        <TextField
+          required
+          id="informationTextField"
+          label="Information"
+          multiline
+          rows={4}
+          fullWidth
+          value={info}
+          onChange={(event) => handleInfoChange(event)} 
+        />
+      </Grid>
+    </React.Fragment>
+                  <div className={classes.buttons}>
+                    {activeStep !== 0 && (
+                      <Button onClick={handleBack} className={classes.button}>
+                        Back
+                      </Button>
+                    )}
+                    
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleNext}
+                      className={classes.button}
+                      disabled={btnDisabled}
+                    >
+                      {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+                    </Button>
+                    
+                  </div>
+                </React.Fragment>
+              )}
+            </React.Fragment>
+          </Paper>
+          
+        </main>
+      </React.Fragment>
     );
-}
+  }
