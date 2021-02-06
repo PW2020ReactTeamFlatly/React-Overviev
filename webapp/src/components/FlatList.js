53ab895a106511e8e89eb9495d720407bddec46e
hiryleu0 <mikoajryll@gmail.com>
2021-02-06 21:09:09 +0100

    better footer

--- webapp/src/components/FlatList.js
+++ webapp/src/components/FlatList.js
@@ -225,12 +225,6 @@ export default function FlatList() {
       </main>
       {/* Footer */}
       <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-                Cos tam w footerze
-        </Typography>
         <Copyright />
       </footer>
       {/* End footer */}
